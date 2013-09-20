3d755f7a7d638a02427bec10312e96a3e430ab73
Julien Delafontaine <julien.delafontaine@epfl.ch>
2013-09-20 14:05:07 +0200

    /bmc/static-media

--- sunny/settings_bbcf.py
+++ sunny/settings_bbcf.py
@@ -60,7 +60,7 @@ MEDIA_ROOT = os.path.join(PROJECT_DIR,'media')
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://example.com/media/", "http://media.example.com/"
-MEDIA_URL = ROOT_URL+'/getfile/'
+MEDIA_URL = '/bmc/getfile/'
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
@@ -70,7 +70,7 @@ STATIC_ROOT = ''
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
-STATIC_URL = ROOT_URL+'/static/'
+STATIC_URL = '/bmc/static/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (

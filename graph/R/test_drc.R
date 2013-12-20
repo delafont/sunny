9ddd30a96010d59ed2db379e75aae8f531281eb8
Julien Delafontaine <julien.delafontaine@epfl.ch>
2013-12-20 17:32:17 +0100

    testing/example scripts

---
+++ graph/R/test_drc.R
@@ -0,0 +1,104 @@
+
+library('drc')
+
+data = read.table("../../sample_data/BIBF_k.txt", header=TRUE)
+
+
+# a is the "scale"
+# b is the "shape"
+# c is the lower bound
+# d is the upper bound
+# e is the inflection point
+
+
+drm(formula = data$response ~ data$dose, fct = LL.2())
+
+# b:(Intercept)  e:(Intercept)
+#        482.4       106836.6
+
+drm(formula = data$response ~ data$dose, fct = LL.3())
+
+# b:(Intercept)  d:(Intercept)  e:(Intercept)
+#        2.297         77.984       2675.295
+
+drm(formula = data$response ~ data$dose, fct = LL.4())
+
+# b:(Intercept)  c:(Intercept)  d:(Intercept)  e:(Intercept)
+#       2.2853        -0.3575        77.9966      2686.8896
+
+drm(formula = data$response ~ data$dose, fct = LL.5())
+
+# b:(Intercept)  c:(Intercept)  d:(Intercept)  e:(Intercept)  f:(Intercept)
+#       3.8198       -25.3687        77.9136      1157.6146         0.1559
+
+drm(formula = data$response ~ data$dose, fct = W1.2())
+
+# b:(Intercept)  e:(Intercept)
+#        246.4       357409.8
+
+drm(formula = data$response ~ data$dose, fct = W1.3())
+
+# b:(Intercept)  d:(Intercept)  e:(Intercept)
+#        1.883         78.041       3379.393
+
+drm(formula = data$response ~ data$dose, fct = W1.4())
+
+# b:(Intercept)  c:(Intercept)  d:(Intercept)  e:(Intercept)
+#        1.920          3.305         78.020       3246.037
+
+drm(formula = data$response ~ data$dose, fct = W2.4())
+
+# b:(Intercept)  c:(Intercept)  d:(Intercept)  e:(Intercept)
+#       -1.190         -9.685         77.929       2148.887
+
+#####################################################################
+
+# Fixed points: estimates disappear!
+
+drm(formula = data$response ~ data$dose, fct = W2.4(fixed = c(NA, 0, NA, NA)))
+
+# b:(Intercept)  d:(Intercept)  e:(Intercept)
+#       -1.465         77.817       1901.889
+
+#####################################################################
+
+model = drm(formula = data$response ~ data$dose, fct = W2.4())
+
+model.summ = summary(model)
+
+# Model fitted: Weibull (type 2) (4 parms)
+#
+# Parameter estimates:
+#
+#                 Estimate Std. Error    t-value p-value
+# b:(Intercept)   -1.19024    0.41770   -2.84954  0.0051
+# c:(Intercept)   -9.68458   15.35835   -0.63057  0.5295
+# d:(Intercept)   77.92899    2.22659   34.99930  0.0000
+# e:(Intercept) 2148.88725  496.34452    4.32943  0.0000
+#
+# Residual standard error:
+#
+#  19.8904 (124 degrees of freedom)
+
+model$
+
+# model$boxcox        model$curveVarNam   model$estMethod     model$origData      model$predres       model$summary
+# model$call          model$data          model$fct           model$parNames      model$robust        model$type
+# model$cm            model$dataList      model$fit           model$parmMat       model$scaleFct      model$varParm
+# model$coefficients  model$deriv1        model$indexMat      model$pfFct         model$start         model$weights
+# model$curve         model$df.residual   model$logDose       model$pmFct         model$sumList
+
+model.summ$
+
+# model.summ$boxcox        model.summ$df.residual   model.summ$resVar        model.summ$text          model.summ$varParm
+# model.summ$coefficients  model.summ$fctName       model.summ$robust        model.summ$type
+# model.summ$cov.unscaled  model.summ$noParm        model.summ$rseMat        model.summ$varMat
+
+#######################################################################
+
+plot(model, type="all", broken=FALSE, xlim=c(0, max(data$dose)), ylim=c(0,100), lty="dashed", lwd=1.3, cex.lab=1.2, cex.main=2, cex=2)
+points(data$dose, fitted(model)[1:length(data$dose)], col='blue')
+model$curve[[1]](seq(10000))
+
+
+
---
+++ graph/R/test_drc.py
@@ -0,0 +1,80 @@
+import rpy2.robjects as ro
+from rpy2.robjects.numpy2ri import numpy2ri
+from rpy2.robjects.packages import importr
+from rpy2.rinterface import RRuntimeError
+from numpy import asarray
+
+drc = importr('drc')
+ro.r("""source("functions.R")""")
+
+
+def list2r(L):
+    """Transform a Python list into a string in R format: [1,2,'C'] -> "c(1,2,'C')" ."""
+    if not isinstance(L,(list,tuple)): L = [L] # everything is a vector in R
+    LL = []
+    for v in L:
+        if isinstance(v,basestring): LL.append("'%s'"%v)
+        elif v is None: LL.append('NA')
+        else: LL.append(str(v))
+    return "c(%s)" % ','.join(LL)
+
+def get_params(model):
+    params = list(model.rx2(2).rx2('par'))
+    return params
+
+def summary(model):
+    info = {}
+    info['fit_fct'] = model.rx2('fct').rx2('name')[0]
+    info['param_names'] = list(model.rx2('fct').rx2('names'))
+    info['coeffs'] = list(model.rx2('coefficients'))
+    info['fixed'] = list(model.rx2('fct').rx2('fixed'))
+    return info
+
+
+param_names = {
+               'LL.2': ['b','e'],
+               'LL.3': ['b','d','e'],
+               'LL.4': ['b','c','d','e'],
+               'LL.5': ['b','c','d','e','f'],
+               'W1.2': ['b','e'],
+               'W1.3': ['b','d','e'],
+               'W1.4': ['b','c','d','e'],
+               'W2.4': ['b','c','d','e'],
+              }
+param_fixed = {
+               'LL.2': {},
+               'LL.3': {},
+               'LL.4': {'c':0},
+               'LL.5': {'c':0},
+               'W1.2': {},
+               'W1.3': {},
+               'W1.4': {'c':0},
+               'W2.4': {'c':0},
+              }
+
+with open("../../sample_data/BIBF_k.txt") as f:
+    header = f.readline()
+    data = [[int(line.split()[0]),float(line.split()[1])] for line in f]
+    dose, response = zip(*data)
+
+ro.r.assign('dose',numpy2ri(asarray(dose)))
+ro.r.assign('response',numpy2ri(asarray(response)))
+
+
+fit_name = "LL.4"
+#fit_fct = ro.r(fit_name+'()')
+fixed = 'fixed='+list2r([0 if x in param_fixed[fit_name] else None for x in param_names[fit_name]])
+fit_fct = ro.r(fit_name+'('+fixed+')')
+
+model = drc.drm(ro.Formula('response~dose'),fct=fit_fct)
+
+# R list - index shifted by one
+# [0]: NULL
+# [1]: $par, $value, $convergence, $message, $hessian, $ovalue
+# [2]: curve()
+# ...
+# [5]: ["b","c","d","e"]
+# [6]: [[predicted values],[residuals]]
+# [7]: call
+# ...
+# [27]: same as [1][0]: parameters
